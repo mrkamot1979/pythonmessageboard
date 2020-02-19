6a6050b7dd1e15af4039fb96ed5fb3ac52b0deff
nino <nino_roldan@yahoo.com>
2020-02-19 15:32:51 +0900

    descriptive posts

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 
+#class below essentially builds a table with the textfield "models"
 class Post(models.Model):
     text = models.TextField()
 
+    def __str__(self):
+        return self.text[:50]
