53450adfc9d12b2a2c884b09ffa3cf6e95aa19de
nino <nino_roldan@yahoo.com>
2020-02-26 16:11:20 +0900

    updated

--- posts/tests.py
+++ posts/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import Post
+from django.urls import reverse #new
 
 # Create your tests here.
 
@@ -15,5 +16,19 @@ class PostModelTest(TestCase):
 
 
 
+class HomePageViewTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text='this is another test')
 
+    def test_view_url_exists_at_proper_location(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
 
+    def test_view_url_by_name(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'home.html')
